128843b69af3cfb48a70e88ab1037e3472f49124
yousef112005 <yousefmostafe37@gmail.com>
2026-02-14 02:43:40 +0200

    Update valantine.js

--- valantine.js
+++ valantine.js
@@ -3,9 +3,11 @@ $(document).ready(function () {
         $('.card').stop().animate({
             top: '-90px'
         }, 'slow');
+        
     }).mouseleave(function () {
         $('.card').stop().animate({
             top: 0
         }, 'slow');
     });
+
 });
